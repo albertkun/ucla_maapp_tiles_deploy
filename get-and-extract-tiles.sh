23b1ad1f68831256b7e6c39395a8d8ce3cc51b14
albertkun <albertk@gmx.com>
2022-10-12 12:01:37 -0700

    feat: test_tiles.zip for git-lfs testing

--- get-and-extract-tiles.sh
+++ get-and-extract-tiles.sh
@@ -6,5 +6,19 @@ for file in ./tiles_to_add/*.zip; do
   unzip -o $file -d ../tiles/file/
 done
 
-for file in ./tiles_to_add/*.zip;
-echo file
+
+# for extracting the tiles
+
+for file in ./tiles_to_add/*.zip; do
+filename=$(basename -- "$file")
+filename="${filename%.*}"
+outdir="../tiles/$filename"
+mkdir -p $outdir
+unzip -q -o $file -d $outdir
+done
+
+# for moving the tiles
+for file in ./tiles_to_add/*.zip; do
+mv $file ./existing_tiles/
+git add . && git commit -m "Added $file tiles" && git push
+done
